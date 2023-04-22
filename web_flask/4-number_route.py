5405846231691236168a70e9b76d580b6dd29929
AymericMbepa <aymericmbepa@gmail.com>
2023-04-22 12:56:36 -0700

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+this module starts a Flask web application that listens on 0.0.0.0, port 5000
+It define a single route at the Root URL ('/') that displays 'Hello HBNB!'
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_hbnb():
+    """
+    this function is the view function for the Root URL ('/')
+    It return the string with a message as a response to the request
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    print 'HBNB'
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Displays 'C ' followed by the value of the text variable"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """Display “Python ”, followed by the value of the text variable"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def n_interger(n):
+    """ display 'n is interger' if n is an integer"""
+    if isinstance(n, int):
+        return "{:d} is integer".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
