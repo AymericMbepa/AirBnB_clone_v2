485f6c6b2cefdba9e122d12f0ba1df7215325e41
AymericMbepa <aymericmbepa@gmail.com>
2023-04-22 08:30:11 -0700

    hello route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+this module starts a Flask web application that listens on 0.0.0.0, port 5000
+It define a single route at the Root URL ('/') that displays 'Hello HBNB!'
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_hbnb():
+    """
+    this function is the view function for the Root URL ('/')
+    It return the string with the message 'Hello HBNB' as a response to the request
+    """
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
