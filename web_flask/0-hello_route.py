d3549e194953b8974057271f4216349f41cc123b
AymericMbepa <aymericmbepa@gmail.com>
2023-04-22 10:29:31 -0700

    hello route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,13 +8,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_hbnb():
     """
     this function is the view function for the Root URL ('/')
-    It return the string with the message 'Hello HBNB' as a response to the request
+    It return the string with a message as a response to the request
     """
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
