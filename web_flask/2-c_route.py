34b91f8a803fac45784b3663944b87de9c40b718
AymericMbepa <aymericmbepa@gmail.com>
2023-04-22 10:59:32 -0700

    c route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+this module starts a Flask web application that listens on 0.0.0.0, port 5000
+It define a single route at the Root URL ('/') that displays 'Hello HBNB!'
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_hbnb():
+    """
+    this function is the view function for the Root URL ('/')
+    It return the string with a message as a response to the request
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    print 'HBNB'
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Displays 'C ' followed by the value of the text variable"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
